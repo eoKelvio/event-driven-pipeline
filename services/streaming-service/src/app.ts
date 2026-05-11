180f442497e5593b8c48c3bdb06751dc206ee45c
eoKelvio <kelvinsc7@gmail.com>
2026-05-11 08:23:36 -0300

    feat(streaming): add app bootstrap and entry point

---
+++ services/streaming-service/src/app.ts
@@ -0,0 +1,16 @@
+import Fastify from 'fastify'
+import mongoPlugin from './plugins/mongo.plugin.js'
+import kafkaPlugin from './plugins/kafka.plugin.js'
+import consumersPlugin from './plugins/consumers.plugin.js'
+import graphqlPlugin from './plugins/graphql.plugin.js'
+
+export async function buildApp() {
+  const app = Fastify({ logger: true })
+
+  await app.register(mongoPlugin)
+  await app.register(kafkaPlugin)
+  await app.register(consumersPlugin)
+  await app.register(graphqlPlugin)
+
+  return app
+}
